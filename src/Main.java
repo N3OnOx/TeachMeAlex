502098abece9e0b145be8dd0190ee0f9e16b095b
Alejandro <alexsanchezarbeit@outlook.com>
2018-04-23 19:00:05 +0200

    fix error

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,37 @@ public class Main {
             opt = sn.nextByte();
             switch (opt){
                 case 1:
-                    palabraController.altaPalabra();
+                    menuAltas();
+                    opt = sn.nextByte();
+                    switch (opt){
+                        case 1:
+                            palabraController.altaPalabra();
+                            break;
+                        case 2:
+                            palabraController.altaVerbo();
+                            break;
+                        default:
+                            System.out.println("Opcion invalida");
+                            break;
+                    }
                     break;
                 case 2:
                     palabraController.bajaPalabra();
                     break;
                 case 3:
-                    palabraController.randomStudy();
+                    menuEstudiar();
+                    opt = sn.nextByte();
+                    switch (opt){
+                        case 1:
+                            palabraController.vocabulary();
+                            break;
+                        case 2:
+                            palabraController.verbs();
+                            break;
+                            default:
+                                System.out.println("Opcion invalida");
+                                break;
+                    }
                     break;
                 case 4:
                     System.out.println("Has elegido salir");
@@ -28,9 +52,18 @@ public class Main {
         }while (opt != 4);
     }
     static void mostrarMenu(){
-        System.out.println("1. Alta palabra");
-        System.out.println("2. Baja palabra");
+        System.out.println("1. Altas");
+        System.out.println("2. Bajas");
         System.out.println("3. Estudiar");
         System.out.println("4. Salir");
+    }
+    static void menuEstudiar(){
+        System.out.println("1. Estudiar vocabulario");
+        System.out.println("2. Estudiar verbos regulares");
+        System.out.println("3. Estudiar verbos irregulares");
+    }
+    static void menuAltas(){
+        System.out.println("1. Alta palabra");
+        System.out.println("2. Alta verbo");
     }
 }
