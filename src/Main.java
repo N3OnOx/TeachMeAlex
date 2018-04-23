d8ee8c182f8c49a789a6c3c6a66b1d8c748e8f7e
Alejandro <alexsanchezarbeit@outlook.com>
2018-04-23 18:42:56 +0200

    fix error

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,64 @@ public class Main {
             opt = sn.nextByte();
             switch (opt){
                 case 1:
-                    palabraController.altaPalabra();
+                    menuAltas();
+                    opt = sn.nextByte();
+                    switch (opt){
+                        case 1:
+                            palabraController.altaPalabra();
+                            break;
+                        case 2:
+                            palabraController.altaVerbo();
+                            break;
+                        default:
+                            System.out.println("Opcion invalida");
+                            break;
+                    }
                     break;
                 case 2:
                     palabraController.bajaPalabra();
                     break;
                 case 3:
-                    palabraController.randomStudy();
+                    menuEstudiar();
+                    opt = sn.nextByte();
+                    switch (opt){
+                        case 1:
+                            palabraController.vocabulary();
+                            break;
+                        case 2:
+                            palabraController.verbs();
+                            break;
+                            default:
+                                System.out.println("Opcion invalida");
+                                break;
+                    }
                     break;
                 case 4:
+                    palabraController.traductor();
+                    break;
+                case 5:
                     System.out.println("Has elegido salir");
                     break;
                     default:
                         System.out.println("Opción inválida");
                         break;
             }
-        }while (opt != 4);
+        }while (opt != 5);
     }
     static void mostrarMenu(){
-        System.out.println("1. Alta palabra");
-        System.out.println("2. Baja palabra");
+        System.out.println("1. Altas");
+        System.out.println("2. Bajas");
         System.out.println("3. Estudiar");
-        System.out.println("4. Salir");
+        System.out.println("4. Traductor");
+        System.out.println("5. Salir");
+    }
+    static void menuEstudiar(){
+        System.out.println("1. Estudiar vocabulario");
+        System.out.println("2. Estudiar verbos regulares");
+        System.out.println("3. Estudiar verbos irregulares");
+    }
+    static void menuAltas(){
+        System.out.println("1. Alta palabra");
+        System.out.println("2. Alta verbo");
     }
 }
--- src/PalabraController.java
+++ src/PalabraController.java
@@ -14,26 +14,70 @@ public class PalabraController {
         String line;
         String diccionario;
         System.out.print("Español --> ");
-        line = sc.nextLine();
-
-        if (!palabraController.existePesp(line)){
-            diccionario = "desp";
-            if (palabraController.existePDiccionario(diccionario,line)){
-                palabra.setEsp(line);
-                System.out.print("English --> ");
-                line = sc.nextLine();
-                diccionario = "ding";
+        line = sc.nextLine().toLowerCase();
+        if (line.startsWith("*")){
+            line = line.substring(1,line.length());
+            palabra.setEsp(line);
+            System.out.print("English --> ");
+            line = sc.nextLine().toLowerCase();
+            palabra.setIng(line);
+            this.palabraController.altaPalabra(palabra);
+        }else {
+            if (!palabraController.existePesp(line)) {
+                diccionario = "desp";
                 if (palabraController.existePDiccionario(diccionario, line)) {
-                    palabra.setIng(line);
-                    this.palabraController.altaPalabra(palabra);
-                }else{
-                    System.out.println("No existe esa palabra en ingles");
+                    palabra.setEsp(line);
+                    System.out.print("English --> ");
+                    line = sc.nextLine().toLowerCase();
+                    diccionario = "ding";
+                    if (palabraController.existePDiccionario(diccionario, line)) {
+                        palabra.setIng(line);
+                        this.palabraController.altaPalabra(palabra);
+                    } else {
+                        System.out.println("No existe esa palabra en ingles");
+                    }
+                } else {
+                    System.out.println("No existe esa palabra en español");
                 }
-            }else{
-                System.out.println("No existe esa palabra en español");
+            } else {
+                System.out.println("Ya existe esa palabra en nuestro vocabulario");
             }
-        }else{
-            System.out.println("Ya existe esa palabra en nuestro vocabulario");
+        }
+    }
+    public void altaVerbo(){
+        Scanner sc = new Scanner(System.in);
+        Palabra palabra = new Palabra();
+        String line;
+        String diccionario;
+        System.out.print("Español --> ");
+        line = sc.nextLine().toLowerCase();
+        if (line.startsWith("*")){
+            line = line.substring(1,line.length());
+            palabra.setEsp(line);
+            System.out.print("English --> ");
+            line = sc.nextLine().toLowerCase();
+            palabra.setIng(line);
+            this.palabraController.altaVerbo(palabra);
+        }else {
+            if (!palabraController.existePesp(line)) {
+                diccionario = "desp";
+                if (palabraController.existePDiccionario(diccionario, line)) {
+                    palabra.setEsp(line);
+                    System.out.print("English --> ");
+                    line = sc.nextLine().toLowerCase();
+                    diccionario = "ding";
+                    if (palabraController.existePDiccionario(diccionario, line)) {
+                        palabra.setIng(line);
+                        this.palabraController.altaVerbo(palabra);
+                    } else {
+                        System.out.println("No existe ese verbo en ingles");
+                    }
+                } else {
+                    System.out.println("No existe ese verbo en español");
+                }
+            } else {
+                System.out.println("Ya existe ese verbo en nuestro vocabulario de verbos");
+            }
         }
     }
     public void bajaPalabra(){
@@ -43,16 +87,47 @@ public class PalabraController {
         palabra = sc.nextLine();
         if (palabraController.existePalabra(palabra)){
             this.palabraController.bajaPalabra(palabra);
+        }else if (palabraController.existeVerbo(palabra)){
+            this.palabraController.bajaVerbo(palabra);
         }else{
             System.out.println("No existe esa palabra");
         }
     }
-    public void randomStudy(){
+    public void traductor(){
+        Scanner sc = new Scanner(System.in);
+        String line;
+        System.out.println("Dime una palabra o verbo: ");
+        line = sc.nextLine();
+        int opt = 0;
+        if (palabraController.existePalabra(line)){
+            opt = 1;
+            System.out.println("******************************");
+            System.out.println("* Español --> "+palabraController.traductor(line,opt).getEsp()+" *");
+            System.out.println("* Ingles --> "+palabraController.traductor(line,opt).getIng()+" *");
+            System.out.println("******************************");
+        }else if (palabraController.existeVerbo(line)){
+            opt = 2;
+            System.out.println("******************************");
+            System.out.println("* Español --> "+palabraController.traductor(line,opt).getEsp()+" *");
+            System.out.println("* Ingles --> "+palabraController.traductor(line,opt).getIng()+" *");
+            System.out.println("******************************");
+        }else{
+            System.out.println("Esa palabra no está en nuestro vocabulario");
+        }
+    }
+
+    public void vocabulary(){
         Scanner sc = new Scanner(System.in);
         String resp;
         boolean checkFallos = true;
         ArrayList<Palabra> fallos = new ArrayList<>();
         int contFallos = 0;
+        long time_start;
+        long time_end = 0;
+        long time;
+        long voca;
+        long verb;
+        time_start = System.currentTimeMillis();
         for (Palabra palabra : this.palabraController.palabrasRandom()) {
             System.out.println("Español --> "+palabra.getEsp());
             System.out.print("English --> ");
@@ -65,6 +140,7 @@ public class PalabraController {
         }
         if (contFallos == 0){
             System.out.println("ENHORABUENA, TODO ACERTADO A LA PRIMERA!!! SIGUE ASÍ!!");
+            time_end = System.currentTimeMillis();
         }else{
             System.out.println("Tiene "+contFallos+" fallos. Desea repetir sus fallos?");
             resp = sc.nextLine();
@@ -85,6 +161,7 @@ public class PalabraController {
                         resp = sc.nextLine();
                         if (!resp.equalsIgnoreCase("si")){
                             System.out.println("Hasta otra!");
+                            time_end = System.currentTimeMillis();
                             contFallos = 0;
                             checkFallos = false;
                         }
@@ -92,10 +169,82 @@ public class PalabraController {
                 }while (contFallos != 0);
                if (checkFallos){
                    System.out.println("Enhorabuena, hemos mitigado los fallos");
+                   time_end = System.currentTimeMillis();
                }
             }else{
                 System.out.println("Hasta otra!");
+                time_end = System.currentTimeMillis();
             }
         }
+        time = palabraController.tiempoEstudio() + (( time_end - time_start )/1000);
+        voca = palabraController.tiempoEstVoca() + (( time_end - time_start )/1000);
+        verb = palabraController.tiempoEstVerb();
+        palabraController.insertarTiempo(time,voca,verb);
     }
+    public void verbs(){
+        Scanner sc = new Scanner(System.in);
+        String resp;
+        boolean checkFallos = true;
+        ArrayList<Palabra> fallos = new ArrayList<>();
+        int contFallos = 0;
+        long time_start;
+        long time_end = 0;
+        long time;
+        long voca;
+        long verb;
+        time_start = System.currentTimeMillis();
+        for (Palabra palabra : this.palabraController.verbosRandom()) {
+            System.out.println("Español --> "+palabra.getEsp());
+            System.out.print("English --> ");
+            resp = sc.nextLine();
+            if (!resp.equalsIgnoreCase(palabra.getIng())){
+                System.out.println("¡¡¡ERROR!!! Ingles --> "+palabra.getIng());
+                contFallos++;
+                fallos.add(palabra);
+            }
+        }
+        if (contFallos == 0){
+            System.out.println("ENHORABUENA, TODO ACERTADO A LA PRIMERA!!!! SIGUE ASÍ!!");
+            time_end = System.currentTimeMillis();
+        }else{
+            System.out.println("Tiene "+contFallos+" fallos. Desea repetir sus fallos?");
+            resp = sc.nextLine();
+            if (resp.equalsIgnoreCase("si")){
+                do {
+                    contFallos = 0;
+                    for (Palabra fallo : fallos) {
+                        System.out.println("Español ---> "+fallo.getEsp());
+                        System.out.print("English ---> ");
+                        resp = sc.nextLine();
+                        if (!resp.equalsIgnoreCase(fallo.getIng())){
+                            System.out.println("¡¡¡ERROR!!! Ingles --> "+fallo.getIng());
+                            contFallos++;
+                        }
+                    }
+                    if (contFallos != 0){
+                        System.out.println("Sigue teniendo fallos, seguimos intentandolo ?");
+                        resp = sc.nextLine();
+                        if (!resp.equalsIgnoreCase("si")){
+                            System.out.println("Hasta otra!!");
+                            time_end = System.currentTimeMillis();
+                            contFallos = 0;
+                            checkFallos = false;
+                        }
+                    }
+                }while (contFallos != 0);
+               if (checkFallos){
+                   System.out.println("Enhorabuena, hemos mitigado los fallos");
+                   time_end = System.currentTimeMillis();
+               }
+            }else{
+                System.out.println("Hasta otra!");
+                time_end = System.currentTimeMillis();
+            }
+        }
+        time = palabraController.tiempoEstudio() + (( time_end - time_start )/1000);
+        voca = palabraController.tiempoEstVoca();
+        verb = palabraController.tiempoEstVerb() + (( time_end - time_start )/1000);
+        palabraController.insertarTiempo(time,voca,verb);
+    }
+
 }
